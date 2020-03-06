e392cbab6ff29d52f744efcedb6fd9bf35c8cd2a
Cody Xie <cody.xie@rock-chips.com>
2020-03-06 23:56:56 +0800

    cmake: use cmake package
    Signed-off-by: Cody Xie <cody.xie@rock-chips.com>

---
+++ cmake/FindGLIB.cmake
@@ -0,0 +1,11 @@
+include(FindPackageHandleStandardArgs)
+
+find_package(PkgConfig REQUIRED)
+
+if(${CMAKE_VERSION} VERSION_LESS "3.6.0")
+    pkg_check_modules(GLIB2 "glib-2.0>=${MIN_GLIB_VERSION}" "gobject-2.0>=${MIN_GLIB_VERSION}" "gio-2.0>=${MIN_GLIB_VERSION}")
+else()
+    pkg_check_modules(GLIB2 IMPORTED_TARGET "glib-2.0>=${MIN_GLIB_VERSION}" "gobject-2.0>=${MIN_GLIB_VERSION}" "gio-2.0>=${MIN_GLIB_VERSION}")
+endif()
+
+find_package_handle_standard_args(GLib DEFAULT_MSG GLIB2_LIBRARIES GLIB2_CFLAGS)
